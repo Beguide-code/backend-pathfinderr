d082bcc5088efbb9551e89686f523ec26b30a17b
Beguide-code <claudebeguide2003@gmail.com>
2025-12-04 13:57:47 -0800

    create a func to handle authentication using jwt

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const Student = require("../models/student");
+
+const protect = async (req,res,next) =>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        try{
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token,process.env.JWT_SECRET);
+            req.student = await Student.findById(decoded.id);
+            next();
+        }
+        catch(error){
+            console.error("Token verification failed",error);
+            res.status(401).json({
+                success:false,
+                error: "Not authorized"
+            });
+        }
+       
+    }
+
+    if(!token){
+        res.status(401).json({
+            success:false,
+            error: "Not authorized, no token"
+        });
+    }
+};
+
+module.exports = {protect};
